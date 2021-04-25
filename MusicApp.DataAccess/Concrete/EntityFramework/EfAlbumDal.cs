3ac1c01e2828a673e258ddeec81843fd8ce79b0e
Adnan Özdemir <XsabriXsabriX@hotmail.com>
2021-04-25 16:55:38 +0300

    EntityFramework data access layers added

---
+++ MusicApp.DataAccess/Concrete/EntityFramework/EfAlbumDal.cs
@@ -0,0 +1,10 @@
+﻿using MusicApp.Core.DataAccess.EntityFramework;
+using MusicApp.DataAccess.Abstract;
+using MusicApp.Entities.Concrete;
+
+namespace MusicApp.DataAccess.Concrete.EntityFramework
+{
+    public class EfAlbumDal:EfEntityRepositoryBase<Album,MusicAppContext>,IAlbumDal
+    {
+    }
+}
