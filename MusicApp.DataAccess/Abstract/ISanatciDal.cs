1c52bdcc8846964e3aca533b9d0dbcf668b17708
Adnan Özdemir <XsabriXsabriX@hotmail.com>
2021-04-25 14:45:52 +0300

    Data Access Layers added

---
+++ MusicApp.DataAccess/Abstract/ISanatciDal.cs
@@ -0,0 +1,9 @@
+﻿using MusicApp.Core.DataAccess;
+using MusicApp.Entities.Concrete;
+
+namespace MusicApp.DataAccess.Abstract
+{
+    public interface ISanatciDal : IEntityRepository<Sanatci>
+    {
+    }
+}
