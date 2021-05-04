2459c97e0d7e5ea247fe22f885f8c4c16f27b122
Adnan Özdemir <XsabriXsabriX@hotmail.com>
2021-05-04 18:39:58 +0300

    Album song's added

--- MusicApp.UI/UserControls/Sections/AlbumlerControl.Designer.cs
+++ MusicApp.UI/UserControls/Sections/AlbumlerControl.Designer.cs
@@ -30,13 +30,17 @@ namespace MusicApp.UI.UserControls.Sections
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.pnlAlbumler = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.pnlSarkilar = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,14 +53,6 @@ namespace MusicApp.UI.UserControls.Sections
             this.label1.Size = new System.Drawing.Size(111, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Albumler";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Location = new System.Drawing.Point(30, 350);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1048, 282);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
             // 
             // label2
             // 
@@ -120,17 +116,72 @@ namespace MusicApp.UI.UserControls.Sections
             this.label6.TabIndex = 4;
             this.label6.Text = "Albüm Tarihi";
             // 
+            // pnlSarkilar
+            // 
+            this.pnlSarkilar.Location = new System.Drawing.Point(30, 381);
+            this.pnlSarkilar.Name = "pnlSarkilar";
+            this.pnlSarkilar.Size = new System.Drawing.Size(1048, 254);
+            this.pnlSarkilar.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(81)))), ((int)(((byte)(251)))));
+            this.label7.Location = new System.Drawing.Point(129, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Şarkı Adı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(81)))), ((int)(((byte)(251)))));
+            this.label8.Location = new System.Drawing.Point(338, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 25);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Sanatçı Adı";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(81)))), ((int)(((byte)(251)))));
+            this.label9.Location = new System.Drawing.Point(508, 353);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 25);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Şarkı Türü";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(81)))), ((int)(((byte)(251)))));
+            this.label10.Location = new System.Drawing.Point(679, 353);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 25);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "İzlenme Sayısı";
+            // 
             // AlbumlerControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(34)))));
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pnlSarkilar);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pnlAlbumler);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "AlbumlerControl";
@@ -144,12 +195,16 @@ namespace MusicApp.UI.UserControls.Sections
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel pnlAlbumler;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel pnlSarkilar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
--- MusicApp.UI/UserControls/Sections/AlbumlerControl.cs
+++ MusicApp.UI/UserControls/Sections/AlbumlerControl.cs
@@ -15,6 +15,7 @@ namespace MusicApp.UI.UserControls.Sections
         private IAlbumDetayService _albumDetayService;
         private ISanatciService _sanatciService;
         private ITurService _turService;
+        private ISarkiService _sarkiService;
 
         private AlbumItem _albumItem;
 
@@ -25,7 +26,32 @@ namespace MusicApp.UI.UserControls.Sections
             _albumDetayService = InstanceFactory.GetInstance<IAlbumDetayService>();
             _sanatciService = InstanceFactory.GetInstance<ISanatciService>();
             _turService = InstanceFactory.GetInstance<ITurService>();
+            _sarkiService = InstanceFactory.GetInstance<ISarkiService>();
         }        
+
+        private void albumSarkilariniGetir(Album album)
+        {
+            pnlSarkilar.Controls.Clear();
+            List<AlbumDetay> albumDetaylari = _albumDetayService.AlbumDetayiGetir(album.albumId);
+            SarkiItem sarkiItem;
+            Sarki sarki;
+            if(albumDetaylari.Count >= 1)
+            {
+                int sayac = 0;
+                foreach (AlbumDetay albumDetay in albumDetaylari)
+                {
+                    sarkiItem = new SarkiItem();
+                    sarki = _sarkiService.SarkiGetir(albumDetay.sarkiId);
+                    sarkiItem.Top = (sayac * 60);
+                    sarkiItem.lblMuzikAdi.Text = sarki.sarkiAdi;
+                    sarkiItem.lblSanatciAdi.Text = _sanatciService.SanatciGetir(albumDetay.sanatciId).sanatciAdi;
+                    sarkiItem.lblTurAdi.Text = _turService.TurGetir(albumDetay.turId).turAdi;
+                    sarkiItem.lblIzlenmeSayisi.Text = sarki.sarkiIzlenme.ToString();
+                    pnlSarkilar.Controls.Add(sarkiItem);
+                    sayac++;
+                }
+            }
+        }
 
         private void albumleriGetir()
         {
@@ -42,6 +68,7 @@ namespace MusicApp.UI.UserControls.Sections
                 _albumItem.lblSanatciAdi.Text = _sanatciService.SanatciGetir(albumDetay[0].sanatciId).sanatciAdi;
                 _albumItem.lblAlbumTarihi.Text = album.albumTarih.ToShortDateString();
                 _albumItem.lblAlbumTuru.Text = _turService.TurGetir(albumDetay[0].turId).turAdi;
+                _albumItem.btnSec.Click += (s, e) => albumSarkilariniGetir(album);
                 pnlAlbumler.Controls.Add(_albumItem);
                 sayac++;
             }
@@ -50,6 +77,7 @@ namespace MusicApp.UI.UserControls.Sections
         private void AlbumlerControl_Load(object sender, EventArgs e)
         {
             Utilities.scroolbarEkle(pnlAlbumler);
+            Utilities.scroolbarEkle(pnlSarkilar);
 
             albumleriGetir();
         }
